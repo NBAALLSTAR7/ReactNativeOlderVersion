c4c34fd156e316fceb8ed48e686191945fba6ddc
subodh <bernetsubo@gmail.com>
2023-06-01 14:11:20 +0545

    first commit

---
+++ components/index.js
@@ -0,0 +1,10 @@
+import HomeHeader from "../components/HomeHeader";
+import FocusedStatusBar from "../components/FocusedStatusBar";
+import NFTCard from "../components/NFTCard"
+import { CircleButton,RectButton } from "./Button";
+import { SubInfo } from "./SubInfo";
+import DetailsDesc from "../components/DetailsDesc";
+import DetailsBid from "../components/DetailsBid"
+export {
+    HomeHeader,FocusedStatusBar,NFTCard,CircleButton,RectButton,DetailsDesc,DetailsBid,SubInfo
+}
